8c12b9dde8eb72ecfec792ed68f1c9c8cd95d9a6
Michelle031 <mbaangel2803@gmail.com>
2022-08-28 16:02:11 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           .catch((err) => console.log(err));
         const name = res.data?.[0].city_name;
         console.log(res);
-        dispatch(setName(res.data.city_name));
+        dispatch(setName(name));
         const res2 = await fetch(
           `https://api.weatherbit.io/v2.0/forecast/daily?city=${name}&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
         )
