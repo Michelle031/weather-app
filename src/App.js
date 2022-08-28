8e91a7389c00041ac7f0a3ff3369859b19244e9d
Michelle031 <mbaangel2803@gmail.com>
2022-08-28 15:34:15 +0100

    error fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 import { useSelector, useDispatch } from "react-redux";
 import { setWeather } from "./features/weatherSlice";
 import { setName } from "./features/locationSlice";
+import { CircularProgress } from "@mui/material";
 
 function App() {
   const { lat, long } = useSelector((state) => state.location);
   const temp = useSelector((state) => state.temp.temp);
   const input = useSelector((state) => state.input.input);
+  const weather = useSelector((state) => state.weather.weather);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,25 +23,14 @@ function App() {
           .then((data) => data.json())
           .catch((err) => console.log(err));
         const name = res.data.city_name;
-        if (input) {
-          dispatch(setName(input));
-          const res2 = await fetch(
-            `https://api.weatherbit.io/v2.0/forecast/daily?city=${input}&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
-          )
-            .then((data) => data.json())
-            .catch((err) => console.log(err));
-          dispatch(setWeather(res2.data));
-        } else {
-          dispatch(setName(name));
-          const res2 = await fetch(
-            `https://api.weatherbit.io/v2.0/forecast/daily?city=${name}&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
-          )
-            .then((data) => data.json())
-            .catch((err) => console.log(err));
-          dispatch(setWeather(res2.data));
-        }
-      }
-      if (!lat || !long) {
+        dispatch(setName(name));
+        const res2 = await fetch(
+          `https://api.weatherbit.io/v2.0/forecast/daily?city=${name}&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
+        )
+          .then((data) => data.json())
+          .catch((err) => console.log(err));
+        dispatch(setWeather(res2.data));
+      } else {
         const res = await fetch(
           `https://api.weatherbit.io/v2.0/forecast/daily?city=Helsinki&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
         )
@@ -48,14 +39,29 @@ function App() {
         dispatch(setName("Helsinki"));
         dispatch(setWeather(res.data));
       }
+      if (input) {
+        dispatch(setName(input));
+        const res2 = await fetch(
+          `https://api.weatherbit.io/v2.0/forecast/daily?city=${input}&key=f50fe99d799c4c3085241e8380601997&days=6&units=${temp}`
+        )
+          .then((data) => data.json())
+          .catch((err) => console.log(err));
+        dispatch(setWeather(res2.data));
+      }
     };
     fetchData();
   }, [dispatch, temp, input]);
 
   return (
     <div className="App h-full flex flex-col md:flex-row ">
-      <Sidebar />
-      <Main />
+      {weather.length === 0 ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Sidebar />
+          <Main />
+        </>
+      )}
     </div>
   );
 }
--- src/lib/getImage.js
+++ src/lib/getImage.js
@@ -1,6 +1,8 @@
 export const getImage = (desc) => {
   if (desc.includes("thunderstorm") || desc.includes("Thunderstorm"))
     return "Thunderstorm";
+  if (desc.includes("shower rain")) return "Shower";
+  if (desc.includes("snow") || desc.includes("Snow")) return "Snow";
   switch (desc) {
     case "Clear Sky":
       return "Clear";
@@ -11,6 +13,10 @@ export const getImage = (desc) => {
     case "Heavy rain":
       return "HeavyRain";
     case "Light rain":
+      return "LightRain";
+    case "Shower rain":
+      return "Shower";
+    case "Moderate rain":
       return "LightRain";
     case "Scattered clouds":
       return "LightCloud";
